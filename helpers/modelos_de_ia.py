7744892a63b584decd0d128142cbbcaef92f5998
Marcos <marcosgab2006@gmail.com>
2025-06-12 15:41:31 -0300

    encerramento dos 3 modelos

--- helpers/modelos_de_ia.py
+++ helpers/modelos_de_ia.py
@@ -1,10 +1,13 @@
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 from sklearn.metrics import classification_report, accuracy_score
 
 def mostrar_resultados(y_test, prev_y, nome_modelo):
       print(f"Acurácia ({nome_modelo}):", accuracy_score(y_test, prev_y))
       print(f"Relatório de Classificação ({nome_modelo}):")
       print(classification_report(y_test, prev_y))
+
 
 def decision_tree(X_train, X_test, y_train, y_test):
       modelo_dt = DecisionTreeClassifier(random_state=42)
@@ -14,3 +17,18 @@ def decision_tree(X_train, X_test, y_train, y_test):
 
       mostrar_resultados(y_test=y_test, prev_y=prev_y, nome_modelo="Decision Tree")
 
+def random_forest(X_train, X_test, y_train, y_test):
+      modelo_rf = RandomForestClassifier(random_state=42)
+
+      modelo_rf.fit(X=X_train, y=y_train)
+      prev_y = modelo_rf.predict(X_test)
+
+      mostrar_resultados(y_test=y_test, prev_y=prev_y, nome_modelo="Random Forest")
+
+def xg_boost(X_train, X_test, y_train, y_test):
+      modelo_xgb = XGBClassifier(use_label_encoder=False, eval_metric='logloss', random_state=42)
+
+      modelo_xgb.fit(X=X_train, y=y_train)
+      prev_y = modelo_xgb.predict(X_test)
+
+      mostrar_resultados(y_test=y_test, prev_y=prev_y, nome_modelo="XGBoost")
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from helpers.preparar_dados import carregar_dataset, dividir_dataset
-from helpers.modelos_de_ia import decision_tree
+from helpers.modelos_de_ia import decision_tree, random_forest, xg_boost
 
 dados = carregar_dataset()
 X = dados[0]
@@ -7,4 +7,6 @@ y = dados[1]
 
 X_train, X_test, y_train, y_test = dividir_dataset(X=X, y=y)
 
-decision_tree(X_train, X_test, y_train, y_test)
+decision_tree(X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test)
+random_forest(X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test)
+xg_boost(X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test)
